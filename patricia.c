296ac13988e0699d2448b9deda59ec396b941151
Bieu <gabrielmmbonfim@gmail.com>
2022-06-16 22:47:20 -0300

    patricia with strings implementation tests

--- patricia.c
+++ patricia.c
@@ -1,138 +1,175 @@
-#include<stdlib.h> 
+#include <stdlib.h> 
+#include <stdio.h>
 #include <sys/time.h>
-#define D 8   /* depende de TipoChave */
 
-typedef unsigned char TipoChave; /* a definir, dependendo da aplicacao */
-typedef unsigned char TipoIndexAmp;
-typedef unsigned char TipoDib;
+#define D 8   /* depende de Palavra */
+
+typedef unsigned char Palavra[64]; /* a definir, dependendo da aplicacao */
+
+typedef unsigned char IndexPalavra;
+
+// typedef unsigned char TipoDib;
+
 typedef enum {
   Interno, Externo
 } TipoNo;
-typedef struct TipoPatNo* TipoArvore; 
-typedef struct TipoPatNo {
-  TipoNo nt;
+
+typedef struct No* Apontador; 
+
+typedef struct No {
+  TipoNo tipo;
   union {
     struct {
-      TipoIndexAmp Index;
-      TipoArvore Esq, Dir;
-    } NInterno ;
-    TipoChave Chave;
-  } NO;
-} TipoPatNo;
+      IndexPalavra index;
+      Apontador esq, dir;
+      char comp;
+    } NoInterno ;
+    Palavra palavra;
+  } No;
+} No;
 
-TipoDib Bit(TipoIndexAmp i, TipoChave k)
-{ /* Retorna o i-esimo bit da chave k a partir da esquerda */
-  int  c, j;
-  if (i == 0)
-  return 0;
-  else { c = k;
-         for (j = 1; j <= D - i; j++) c /= 2;
-         return (c & 1);
-       }
-} 
-
-short EExterno(TipoArvore p)
+short VerifExterno(Apontador p)
 { /* Verifica se p^ e nodo externo */
-  return (p->nt == Externo);
+  return (p->tipo == Externo);
 }
 
-TipoArvore CriaNoInt(int i, TipoArvore *Esq,  TipoArvore *Dir)
-{ TipoArvore p;
-  p = (TipoArvore)malloc(sizeof(TipoPatNo));
-  p->nt = Interno; p->NO.NInterno.Esq = *Esq;
-  p->NO.NInterno.Dir = *Dir; p->NO.NInterno.Index = i; return p;
+Apontador NoInternoCriar(int i, Apontador *esq,  Apontador *dir, char comp)
+{ 
+  Apontador p;
+  p = (Apontador)malloc(sizeof(No));
+  p->tipo = Interno; 
+  p->No.NoInterno.esq = *esq;
+  p->No.NoInterno.dir = *dir; 
+  p->No.NoInterno.comp = comp; 
+  p->No.NoInterno.index = i; 
+  return p;
 } 
 
-TipoArvore CriaNoExt(TipoChave k)
-{ TipoArvore p;
-  p = (TipoArvore)malloc(sizeof(TipoPatNo));
-  p->nt = Externo; p->NO.Chave = k; return p;
+Apontador NoExternoCriar(Palavra palavra)
+{ 
+  Apontador p;
+  p = (Apontador)malloc(sizeof(No));
+  p->tipo = Externo; 
+  strcpy(p->No.palavra, palavra);
+  return p;
 }  
 
-void Pesquisa(TipoChave k, TipoArvore t)
-{ if (EExterno(t)) 
-  { if (k == t->NO.Chave) 
-    printf("Elemento encontrado\n");
-    else printf("Elemento nao encontrado\n");
-    return;
+void Pesquisar(Palavra palavra, Apontador t)
+{
+  if (VerifExterno(t)) {
+    printf("A palavra <%s> foi encontrada!", t->No.palavra);
+
+    if (strcmp(palavra, t->No.palavra) != 0) {
+      printf("Palavra nao encontrada na arvore");
+      return;
+    } else {
+      printf("A palavra <%s> foi encontrada!", palavra);
+      return;
+    }
   }
-  if (Bit(t->NO.NInterno.Index, k) == 0) 
-  Pesquisa(k, t->NO.NInterno.Esq);
-  else Pesquisa(k, t->NO.NInterno.Dir);
+
+  printf("\n%c ", t->No.NoInterno.comp);
+  printf(" %d ", t->No.NoInterno.index);
+  printf(" %c\n", palavra[t->No.NoInterno.index]);
+
+  if (strlen(palavra) < t->No.NoInterno.index) {
+    return Pesquisar(palavra, t->No.NoInterno.esq);
+  } else if (palavra[t->No.NoInterno.index] <= t->No.NoInterno.comp) {
+    return Pesquisar(palavra, t->No.NoInterno.esq);
+  } else {
+    return Pesquisar(palavra, t->No.NoInterno.dir);
+  }
 } 
 
-TipoArvore InsereEntre(TipoChave k, TipoArvore *t, int i)
-{ TipoArvore p;
-  if (EExterno(*t) || i < (*t)->NO.NInterno.Index) 
-  { /* cria um novo no externo */
-    p = CriaNoExt(k);
-    if (Bit(i, k) == 1) 
-    return (CriaNoInt(i, t, &p));
-    else return (CriaNoInt(i, &p, t));
-  } 
-  else 
-  { if (Bit((*t)->NO.NInterno.Index, k) == 1)
-    (*t)->NO.NInterno.Dir = InsereEntre(k,&(*t)->NO.NInterno.Dir,i);
-    else
-    (*t)->NO.NInterno.Esq = InsereEntre(k,&(*t)->NO.NInterno.Esq,i);
+Apontador InserirEntre(Palavra palavra, Apontador *t, int i, char comp)
+{ 
+  Apontador p;
+  if (VerifExterno(*t)) 
+  { 
+    p = NoExternoCriar(palavra);
+    if (strcmp((*t)->No.palavra, palavra) > 0) {
+      return (NoInternoCriar(i, &p, t, comp));
+    } else if (strcmp((*t)->No.palavra, palavra) < 0) {
+      return (NoInternoCriar(i, t, &p, comp));
+    } 
+  } else if (i < (*t)->No.NoInterno.index) {
+    p = NoExternoCriar(palavra);
+    if (palavra[i] < comp)
+    { 
+      return (NoInternoCriar(i, &p, t, comp));
+    } else {
+      return (NoInternoCriar(i, t, &p, comp));
+    }
+  } else { 
+    int newIndex = (*t)->No.NoInterno.index;
+
+    if(palavra[newIndex] > (*t)->No.NoInterno.comp) {
+      (*t)->No.NoInterno.esq = InserirEntre(palavra, &(*t)->No.NoInterno.esq, i, comp);
+    } else {
+      (*t)->No.NoInterno.dir = InserirEntre(palavra, &(*t)->No.NoInterno.dir, i, comp);
+    }
     return (*t);
   }
 }
 
-TipoArvore Insere(TipoChave k, TipoArvore *t)
-{ TipoArvore p;
+Apontador Inserir(Palavra palavra, Apontador *t)
+{ 
+  Apontador p;
   int i;
-  if (*t == NULL) 
-  return (CriaNoExt(k));
-  else 
-  { p = *t;
-    while (!EExterno(p)) 
-      { if (Bit(p->NO.NInterno.Index, k) == 1)
-        p = p->NO.NInterno.Dir;
-        else p = p->NO.NInterno.Esq;
+  if (*t == NULL) {
+    return (NoExternoCriar(palavra));
+  } else { 
+    p = *t;
+    while (!VerifExterno(p)) { 
+      if (palavra[p->No.NoInterno.index] <= p->No.NoInterno.comp) {
+        p = p->No.NoInterno.esq;
+      } else if (palavra[p->No.NoInterno.index] > p->No.NoInterno.comp) {
+        p = p->No.NoInterno.dir;
       }
-    /* acha o primeiro bit diferente */
-    i = 1;
-    while ((i <= D) & (Bit((int)i, k) == Bit((int)i, p->NO.Chave))) 
-      i++;
-    if (i > D) 
-    { printf("Erro: chave ja esta na arvore\n");  return (*t); } 
-    else return (InsereEntre(k, t, i));
+    }
+    
+    if (strcmp(palavra, p->No.palavra) != 0) {
+      int tam1 = strlen(palavra), tam2 = strlen(p->No.palavra), tam;
+      if (tam1 < tam2) {
+        tam = tam1;
+      } else {
+        tam = tam2;
+      }
+      
+      char comp;
+
+      for (i = 0; i < tam; i++) {
+        if (palavra[i] != p->No.palavra[i]) {
+          if (palavra[i] <= p->No.palavra[i]) {
+            comp =  p->No.palavra[i];
+          } else {
+            comp =  palavra[i];
+          }
+        }
+      }
+      return InserirEntre(palavra, t, i, comp);
+    } else {
+      printf("A palavra <%s> ja foi inserida", palavra);
+      return (*t);
+    }
+    
+
   }
 }
 
 int main(int argc, char *argv[])
-{ TipoArvore a = NULL;
-  TipoChave c;
-  int  i, j, k, n;
-  int  min = 32, max = 126;
-  TipoChave vetor[95];
-  /* Gera uma permutacao aleatoria de chaves dos caracteres ASCII 32 a  126 */
-  struct timeval semente;
-  gettimeofday(&semente,NULL);
-  srand((int)(semente.tv_sec + 1000000 * semente.tv_usec));  
-  for (i = min; i <= max; i++)
-  vetor[i - 32] = i;
-  for (i = min; i <= max; i++) 
-    { k = min + (int) ((float)(max - min) * rand()/(RAND_MAX + 1.0)); 
-      j = min + (int) ((float)(max - min) * rand()/(RAND_MAX + 1.0));
-      n = vetor[k - 32]; vetor[k - 32] = vetor[j - 32]; vetor[j - 32] = n; 
-    }
-  /* Insere cada chave na arvore */
-  for (i = min; i <= max; i++) 
-    { c = vetor[i - 32]; printf("Inserindo chave: %c\n", c);
-      a = Insere(c, &a);
-    }
-  /* Gera outra permutacao aleatoria de chaves */
-  for (i = min; i <= max; i++) 
-    { k = min + (int) ((float)(max-min) * rand()/(RAND_MAX + 1.0));
-      j = min + (int) ((float)(max-min) * rand()/(RAND_MAX + 1.0));
-      n = vetor[k - 32]; vetor[k - 32] = vetor[j - 32]; vetor[j - 32] = n;
-    }
-  /* Pesquisa cada chave na arvore */
-  for (i = min; i <= max; i++) 
-    { c = vetor[i - 32]; printf("Pesquisando chave: %c\n", c);
-      Pesquisa(c, a);
-    }
+{ 
+  Apontador a = NULL;
+  Palavra c;
+
+  a = Inserir("rato", &a);
+  a = Inserir("rataria", &a);
+  a = Inserir("roeu", &a);
+  a = Inserir("roupa", &a);
+  // a = Inserir("rei", &a);
+  // a = Inserir("roma", &a);
+  // a = Inserir("ratos", &a);
+  Pesquisar("roeu", a);
+
   return 0;
 } 
